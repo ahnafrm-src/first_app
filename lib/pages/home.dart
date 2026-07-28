b2566d2376e4483054a7764cd168152e4a4e0da7
akmwid <anjaymabar0173@gmail.com>
2026-07-28 11:16:57 +0700

    feat: tampilan chat wa

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../element/button.dart';
 import '../element/image_network.dart';
 import '../element/listview.dart';
+import '../element/chatWa.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -18,15 +19,25 @@ class HomePage extends StatelessWidget {
           padding: EdgeInsetsGeometry.only(left: 10, bottom: 5),
           child: ClipRRect(
             borderRadius: BorderRadius.circular(55),
-            child: ImageNetwork(isBox: true, urlImg: "https://i.pinimg.com/736x/57/35/66/573566386f3f2f5d9d8b04700dc4cf92.jpg",),
+            child: ImageNetwork(
+              isBox: true,
+              urlImg:
+                  "https://i.pinimg.com/736x/57/35/66/573566386f3f2f5d9d8b04700dc4cf92.jpg",
+            ),
           ),
         ),
       ),
       body: Container(
         width: size.width,
         height: size.height,
+        padding: EdgeInsets.all(10),
         decoration: BoxDecoration(color: Colors.green[400]),
-        child: Listview(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [SizedBox(height: 200, width: size.width, child: Listview(),),
+          Chatwa()
+          ],
+        ),
       ),
       floatingActionButton: ButtonHome(),
     );
